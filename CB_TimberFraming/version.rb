458e3875f3a4f7f24110d82dff1a750df031ceb8
Clark Bremer <clarkbremer@gmail.com>
2024-03-28 13:41:05 -0500

    Bump version

--- CB_TimberFraming/version.rb
+++ CB_TimberFraming/version.rb
@@ -1,5 +1,5 @@
 module CB_TF
   base_name = File.dirname(__FILE__).split("/").last
-  const_set("#{base_name}_VERSION", "3.0.7")
-  const_set("#{base_name}_DATE", "03/26/2024")
+  const_set("#{base_name}_VERSION", "3.0.8")
+  const_set("#{base_name}_DATE", "03/28/2024")
 end
