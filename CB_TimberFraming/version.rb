38d1146554874dd7c0c5b062487e0f4dba83969c
Clark Bremer <clarkbremer@gmail.com>
2024-04-16 16:08:24 -0500

    Version 3.0.16

--- CB_TimberFraming/version.rb
+++ CB_TimberFraming/version.rb
@@ -1,5 +1,5 @@
 module CB_TF
   base_name = File.dirname(__FILE__).split("/").last
-  const_set("#{base_name}_VERSION", "3.0.13")
-  const_set("#{base_name}_DATE", "04/08/2024")
+  const_set("#{base_name}_VERSION", "3.0.16")
+  const_set("#{base_name}_DATE", "04/16/2024")
 end
